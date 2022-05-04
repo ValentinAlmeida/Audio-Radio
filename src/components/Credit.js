5b4b1e287e1d0fafb10bd8d12ad7c1196def1265
ValentinAlmeida <valentinfalmeida@hotmail.com>
2022-05-04 11:03:38 -0300

    ready

---
+++ src/components/Credit.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+const Credit = () => {
+	return (
+		<CreditContainer>
+			<Link href="" target="_blank">
+				IMTECH
+			</Link>
+			<br></br>
+			<Link href="" target="_blank">
+				SAMU MAIS
+			</Link>
+		</CreditContainer>
+	);
+};
+
+const CreditContainer = styled.div`
+	user-select: none;
+	position: fixed;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+	justify-content: flex-end;
+	z-index: 12;
+	bottom: 5px;
+	right: 5px;
+	color: rgb(155, 155, 155);
+	font-size: 0.75rem;
+`;
+
+const Link = styled.a`
+	color: rgb(155, 155, 155);
+`;
+export default Credit;
